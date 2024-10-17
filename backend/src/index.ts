5edb47db4e41416db9dacdec367032582acdd6f9
Sivasish Praharaj <sivasish48@gmail.com>
2024-10-17 20:58:54 +0530

    added some requied imports

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("working fine");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running at , http://localhost:5000");
+});
